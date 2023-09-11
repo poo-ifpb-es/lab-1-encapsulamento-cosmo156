0a0537140eda5595f03f751fdf1cac774707921a
cosmo156 <cosmodesouzapess@gmail.com>
2023-09-11 16:20:32 -0300

    Tudo corrigido (rec)

--- src/main/java/CarroAluguel.java
+++ src/main/java/CarroAluguel.java
@@ -3,84 +3,92 @@ public class CarroAluguel {
     private float distanciaPercorrida;
     private boolean disponivel;
     private boolean sinistro;
-    private float debito;
 
-    public CarroAluguel(float valorPorKm) {
+    public CarroAluguel(float valorPorKm){
         this.valorPorKm = valorPorKm;
-        this.distanciaPercorrida = 0.0f;
+        this.distanciaPercorrida = 0;
         this.disponivel = true;
+    }
+
+    public float getValorPorKm(){
+        return this.valorPorKm;
+    }
+
+    public void setValorPorKm(float valorPorKm){
+        this.valorPorKm = valorPorKm;
+    }
+
+    public float getDistanciaPercorrida(){
+        return this.distanciaPercorrida;
+    }
+
+    public void setDistanciaPercorrida(float distanciaPercorrida){
+        this.distanciaPercorrida = distanciaPercorrida;
+    }
+
+    public void alugar() throws CarroIndisponivelException {
+        if(!isDisponivel()) {
+            throw new CarroIndisponivelException();
+        }
+
+        this.disponivel = false;
+    }
+
+    public void devolver() throws CarroDisponivelException, CarroNaoPagoException  {
+        if(isDisponivel()){
+            throw new CarroDisponivelException();
+        } else if(getDebito() != 0){
+            throw new CarroNaoPagoException();
+        }
+
         this.sinistro = false;
-        this.debito = 0.0f;
+        this.distanciaPercorrida = 0;
+        this.disponivel = true;
     }
-    public void setSinistro(boolean sinistro) {
+
+    public boolean isDisponivel(){
+        return this.disponivel;
+    }
+
+    public boolean hasSinistro(){
+        return this.sinistro;
+    }
+
+    public void setSinistro(boolean sinistro){
         this.sinistro = sinistro;
-    }
-    public void setValorPorKm(float valor) {
-        this.valorPorKm = valor;
-    }
-
-    public float getValorPorKm() {
-        return valorPorKm;
-    }
-
-    public void setDistanciaPercorrida(float distancia) throws CarroIndisponivelException {
-        if (disponivel) {
-            this.distanciaPercorrida = distancia;
-        } else {
-            throw new CarroIndisponivelException("O carro está indisponível.");
-        }
-    }
-
-    public float getDistanciaPercorrida() {
-        return distanciaPercorrida;
-    }
-
-    public void alugar() throws CarroIndisponivelException{
-        if (disponivel) {
-            disponivel = false;
-        } else {
-            throw new CarroIndisponivelException("O carro está indisponível.");
-        }
-    }
-
-    public void devolver() throws CarroNaoPagoException, CarroDisponivelException {
-        if (!disponivel) {
-            if (debito == 0.0f) {
-                disponivel = true;
-                sinistro = false;
-                distanciaPercorrida = 0;
-            } else {
-                throw new CarroNaoPagoException("O carro não foi pago.");
-            }
-        } else {
-            throw new CarroDisponivelException("O carro está disponível.");
-        }
-    }
-
-    public boolean isDisponivel() {
-        return disponivel;
-    }
-
-    public boolean hasSinistro() {
-        return sinistro;
     }
 
     public float getDebito() {
-        if (sinistro) {
-            return debito + (0.6f * debito); // Adiciona 60% ao valor do débito em caso de sinistro
-        } else {
-            return debito;
+        float valor = valorPorKm * distanciaPercorrida;
+        if(hasSinistro()){
+            return valor + 0.6f * valor;
         }
+
+        return valor;
     }
 
-    public void pagar() throws CarroDisponivelException{
-        if (this.disponivel==false) {
-            distanciaPercorrida = 0.0f;
-            sinistro = false;
-        } else {
-            throw new CarroDisponivelException("O carro está disponível.");
-        }
+    public void pagar() throws CarroDisponivelException {
+        if(isDisponivel()) {
+            throw new CarroDisponivelException();
+        } 
+
+        this.distanciaPercorrida = 0;
     }
 }
 
+class CarroDisponivelException extends Exception {
+    public CarroDisponivelException() {
+        super("O carro está disponível.");
+    }
+}
+class CarroIndisponivelException extends Exception{
+    public CarroIndisponivelException() {
+        super("O carro está indisponível.");
+    }
 
+}
+class CarroNaoPagoException extends Exception{
+    public CarroNaoPagoException() {
+        super("O carro não foi pago.");
+    }
+}
